37dd35781b936739d43370d915b27ab4213daa58
Golamari16 <mahithareddygolamari@gmail.com>
2026-05-10 23:21:45 +0530

    Added datatypes programs

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args){
+      //-128 to 127
+       byte b=127;
+       // byte b=128 -- invalid
+       float x=0.1f;
+       double x1=0.1;
+       double x2=0.2;
+       System.out.println(x1+x2);
+       char c='\u0041';
+        System.out.println(c);
+        char ch='A';
+        System.out.println(ch);
+        char cha=65;
+        System.out.println(cha);
+    }
+}
